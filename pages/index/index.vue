ef368b0cd07fd497164ac3957007acc6d394c897
unknown <1316923633@qq.com>
2020-07-11 22:52:06 +0800

    小程序分享功能制作

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,8 @@
 <template>
 	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<button type="default" @click="clickMe">测试</button>
-		<uni-link :href="href" :text="href"></uni-link>
+		<view><text>分享必须要用button 且 属性里面要有 open-type="share" ，uni-app 的api只支持app的分享，小程序的不支持 </text></view>
+		<button type="default" open-type="share">分享例子</button>
+
 	</view>
 </template>
 
@@ -11,13 +10,41 @@
 	export default {
 		data() {
 			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+
+			}
+		},
+
+		onShareAppMessage: function(res) {
+			var that = this;
+			if (res.from === 'button') {
+				console.log('来自页面内转发按钮');
+			} else if (res.from === 'menu') {
+				console.log('右上角菜单转发按钮');
+			}
+			// 返回数据
+			return {
+				title: '我在江湖',
+				path: 'pages/share/share',
+				imageUrl:'http://slyx.17w.vip/game/static/scrollImage/%E6%88%91%E5%9C%A8%E6%B1%9F%E6%B9%962.jpg',
+				success: function(res) {
+					// 转发成功，可以把当前页面的链接发送给后端，用于记录当前页面被转发了多少次或其他业务
+					wx.request({
+						url: app.buildUrl("/member/share"),
+						data: {
+							url: utils.getCurrentPageUrlWithArgs()
+						},
+						success: function(res) {
+							console.log('分享成功');
+						}
+					});
+				},
+				fail: function(res) {
+					console.log('分享失败');
+				}
 			}
 		},
 		methods: {
-			clickMe:function(){
-				console.log('测试');
-			},
+
 
 		}
 	}
---
+++ pages/share/share.vue
@@ -0,0 +1,10 @@
+<template>
+	<vie>我在江湖 分享成功 这里放个webView</vie>
+	
+</template>
+<
+<script>
+</script>
+
+<style>
+</style>
